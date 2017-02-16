ae1e49a7a97d4a248d4296f7f5b5811ece9991d3
Joel89 <astrand.joel@gmail.com>
2017-02-16 12:38:05 +0100

    Added

--- pages/menuScript.js
+++ pages/menuScript.js
@@ -6,12 +6,97 @@
  * Created by Joel_ on 2017-02-07.
  */
 
+/* ----------------------------Show the menu from the database functions------------------------------------------*/
 /* Gör så att hemsidan laddas först innan JQuery sätter igång.
  */
+function docLoaded(fn) {
+    if (document.readyState !== 'loading'){
+        fn();
+    } else {
+        document.addEventListener('DOMContentLoaded', fn);
+    }
+}
 
+function indexPageLoaded() {
+    /*displayItems(); */
+    beer();
+}
+
+
+/* Funktion som hämtar hela sortimentet från databasen. Använder admin namnet ervtod för att få tillgång till
+ databasen och funktionen inventory_get för att retunera sortimentet.  Anropar en annan funktonen med databasens
+ data som är i JSON format.
+ */
+function beer() {
+    $.ajax({
+        type:'GET',
+        url:'http://pub.jamaica-inn.net/fpdb/api.php?username=ervtod&password=ervtod&action=inventory_get',
+        dataType:'json',
+        success:function (data) {
+            parseBeer(data.payload);
+        }
+    });
+}
+
+
+
+/* beers är sortimentet från databasen och innehåller 167 olika drycker. */
+function parseBeer(beers)  {
+    // create a new div element
+    // and give it some content
+    for (i = 0; i<beers.length; i++) {
+
+        /* skapar en ny div för varje loop för att göra listan dynamisk. */
+        /* måste använda en ny break element varje gång man använder den eftersom element inte kan upprepas*/
+
+        var newDiv = document.createElement("div");
+        /* Skapar ett class name så att man kan ändra divens utseende i css filen.*/
+        newDiv.className = "divdrag";
+
+
+        var para = document.createElement("p");
+        var br = document.createElement("br");
+        var br2 = document.createElement("br");
+        var br3 = document.createElement("br");
+
+
+        /* Sorterar ut de värden som ej har något namn i databasen.*/
+
+        if(beers[i].namn != "")
+        {
+            /* Plockar ut värdena från databasen.*/
+            var nodeName = document.createTextNode(beers[i].namn);
+            var pub_price = document.createTextNode("Price: " + beers[i].pub_price);
+
+            /*Sätter fast värdena i en paragraf element och använder break för att få varje element på ny rad.*/
+            para.appendChild(nodeName);
+            para.appendChild(br);
+            para.appendChild(br2);
+            para.appendChild(pub_price);
+
+            newDiv.appendChild(para); //add the text node to the newly created div.
+
+            // add the newly created element and its content into the DOM
+            /*
+             var currentDiv = document.getElementById("div1");
+             document.body.insertBefore(newDiv, currentDiv);
+             */
+            var div2 = document.getElementById("childDiv");
+            var parentDiv = div2.parentNode;
+            parentDiv.insertBefore(newDiv, div2);
+
+        }
+
+    }
+}
+
+/* ---------------------------------------------------------------------------------------------------------------*/
+
+
+/* ----------------------------Drag and Drop functions------------------------------------------*/
 $(document).ready(function () {
 
-    $(".block").draggable({helper:'clone'});
+    $(".divdrag").draggable({helper:'clone'});
 
     $("#orderlistBox").droppable(
         {
@@ -25,51 +110,5 @@ $(document).ready(function () {
 });
 
 
-/**
- * Created by Joel_ on 2017-02-03.
- */
 
-/* Objekt konstruktor */
-function menu(drinkName, drinkIngredients, drinkStyrka,drinkVolym, drinkSrc,
-              drinkSerieNummer) {
-    this.Name = drinkName;
-    this.ingredients = drinkIngredients;
-    this.styrka = drinkStyrka;
-    this.Volym =drinkVolym;
-    this.imageSrc = drinkSrc;
-    this.serieNummer = drinkSerieNummer;
-}
-
-/*Skapar 5 st nya element */
-var drink1 = new menu('Beer', 'Lactose,gluten', 12, 300, "../css/beermug.jpg",1);
-
-function addDrink(drink1) {
-
-
-    var OrderName = document.createTextNode(drink1.Name);
-    var Orderingredients = document.createTextNode(drink1.ingredients);
-    var Orderstyrka = document.createTextNode(drink1.styrka);
-    var OrderVolym = document.createTextNode(drink1.Volym);
-
-    var img = new Image(50, 50);
-    img.src = drink1.imageSrc;
-
-    /*Skapar en node som man fäster texten vid.*/
-
-    var drinkName = document.getElementById("OrderName");
-    var drinkBild = document.getElementById("OrderBild");
-    var drinkingredients = document.getElementById("Orderingredients");
-    var drinkstyrka = document.getElementById("Orderstyrka");
-    var drinkvolym = document.getElementById("OrderVolym");
-
-    /*lägger till texten vid id i htlm*/
-
-    drinkName.appendChild(OrderName);
-    drinkBild.appendChild(img);
-    drinkingredients.appendChild(Orderingredients);
-    drinkstyrka.appendChild(Orderstyrka);
-    drinkvolym.appendChild(OrderVolym);
-}
-
-
-addDrink(drink1);
+/* ---------------------------------------------------------------------------------------------------------------*/
