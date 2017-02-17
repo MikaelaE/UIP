d7701179a483da243f4ec5a45d98cd7ff36dc5f2
Joel89 <astrand.joel@gmail.com>
2017-02-17 13:21:25 +0100

    Added drag and Drrop, comments, new page payment

--- pages/menuScript.js
+++ pages/menuScript.js
@@ -5,10 +5,6 @@
 /**
  * Created by Joel_ on 2017-02-07.
  */
-
-/* ----------------------------Show the menu from the database functions------------------------------------------*/
-/* Gör så att hemsidan laddas först innan JQuery sätter igång.
- */
 function docLoaded(fn) {
     if (document.readyState !== 'loading'){
         fn();
@@ -16,99 +12,96 @@ function docLoaded(fn) {
         document.addEventListener('DOMContentLoaded', fn);
     }
 }
-
+/* Function that start the page and calls the neccessary functions. */
 function indexPageLoaded() {
-    /*displayItems(); */
-    beer();
+
+    getTheInventoryDatabase();
 }
 
 
-/* Funktion som hämtar hela sortimentet från databasen. Använder admin namnet ervtod för att få tillgång till
- databasen och funktionen inventory_get för att retunera sortimentet.  Anropar en annan funktonen med databasens
- data som är i JSON format.
+/* Function that retrieves all alcohol products from the database. The function use
+ the admin name ervtod to get access to the database and the function inventory_get to
+ retrieve the database. getTheInventoryDatabase() calls another function with the database data
+ as paramter which is in JSON Format.
  */
-function beer() {
+
+function getTheInventoryDatabase() {
     $.ajax({
         type:'GET',
         url:'http://pub.jamaica-inn.net/fpdb/api.php?username=ervtod&password=ervtod&action=inventory_get',
         dataType:'json',
         success:function (data) {
-            parseBeer(data.payload);
+            printOutTheMenu(data.payload);
         }
     });
 }
 
 
 
-/* beers är sortimentet från databasen och innehåller 167 olika drycker. */
-function parseBeer(beers)  {
-    // create a new div element
-    // and give it some content
+
+/*
+ The function is getting the inventory database and adding it to the page.
+ Parameter: The inventory database which is in JSON format.
+ */
+function printOutTheMenu(beers)  {
+    /*creates dynamically so many divs as the number of elements in beers. */
+
     for (i = 0; i<beers.length; i++) {
 
-        /* skapar en ny div för varje loop för att göra listan dynamisk. */
-        /* måste använda en ny break element varje gång man använder den eftersom element inte kan upprepas*/
-
         var newDiv = document.createElement("div");
-        /* Skapar ett class name så att man kan ändra divens utseende i css filen.*/
-        newDiv.className = "divdrag";
 
+        /* Give the new div a class so we can manipulate the div in the css file. */
+        newDiv.className = "dynamicallyDivClass";
 
-        var para = document.createElement("p");
+        /*Create a paragraph element to hold the data from the database.*/
+        var paragraph = document.createElement("p");
+
+        /* Create some breaking point so every sentence comes into a new row. Can't use a element
+         * more than once therefore the the need of many break. */
         var br = document.createElement("br");
         var br2 = document.createElement("br");
-        var br3 = document.createElement("br");
 
-
-        /* Sorterar ut de värden som ej har något namn i databasen.*/
-
+        /* Add only the element wiht name.(The current database has a few element without names)*/
         if(beers[i].namn != "")
         {
-            /* Plockar ut värdena från databasen.*/
+            /* Get the data from the database and create element from them.*/
             var nodeName = document.createTextNode(beers[i].namn);
             var pub_price = document.createTextNode("Price: " + beers[i].pub_price);
 
-            /*Sätter fast värdena i en paragraf element och använder break för att få varje element på ny rad.*/
-            para.appendChild(nodeName);
-            para.appendChild(br);
-            para.appendChild(br2);
-            para.appendChild(pub_price);
+            /* Set the data to the paragraph node. Using the break point to get the data on a new row.*/
+            paragraph.appendChild(nodeName);
+            paragraph.appendChild(br);
+            paragraph.appendChild(pub_price);
 
-            newDiv.appendChild(para); //add the text node to the newly created div.
+            /* Add the text node to the newly created div.*/
+            newDiv.appendChild(paragraph);
 
-            // add the newly created element and its content into the DOM
-            /*
-             var currentDiv = document.getElementById("div1");
-             document.body.insertBefore(newDiv, currentDiv);
-             */
+            /* Add the newly created element and its content into the DOM. */
             var div2 = document.getElementById("childDiv");
             var parentDiv = div2.parentNode;
             parentDiv.insertBefore(newDiv, div2);
 
         }
 
+        /* Need to make the draggable and droppable functions here, if they is outside the function
+         the drag and drop ability will go away after reloaded the page.
+         http://stackoverflow.com/questions/4225341/draggable-stops-working-after-i-reload-a-part-of-web-page-with-ajax
+         */
+        $(".dynamicallyDivClass").draggable({helper:'clone'});
+
+        $("#orderlistBox").droppable(
+            {
+                drop:function (ev,ui) {
+                    ev.preventDefault();
+                    var droppedItem = $(ui.draggable).clone();
+                    $(this).append(droppedItem);
+
+                }
+            }
+        );
+
     }
 }
 
 /* ---------------------------------------------------------------------------------------------------------------*/
 
-
-/* ----------------------------Drag and Drop functions------------------------------------------*/
-$(document).ready(function () {
-
-    $(".divdrag").draggable({helper:'clone'});
-
-    $("#orderlistBox").droppable(
-        {
-            accept:".block",
-            drop:function (ev,ui) {
-                var droppedItem = $(ui.draggable).clone();
-                $(this).append(droppedItem);
-            }
-        }
-    );
-});
-
-
-
-/* ---------------------------------------------------------------------------------------------------------------*/
