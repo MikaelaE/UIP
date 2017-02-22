91c43ae2809055d9a3b402a2e6203810823afa53
Joel89 <astrand.joel@gmail.com>
2017-02-22 14:43:36 +0100

    The log out button NO is working for all user

--- js/AdminIndexScript.js
+++ js/AdminIndexScript.js
@@ -1,6 +1,7 @@
 /**
- * Created by Joel_ on 2017-02-21.
+ * Created by Joel_ on 2017-02-22.
  */
+
 function docLoaded(fn) {
     if (document.readyState !== 'loading'){
         fn();
@@ -11,9 +12,7 @@ function docLoaded(fn) {
 /* Function that start the page and calls the necessary functions. */
 function indexPageLoaded() {
     getTheUserDatabase();
-    getTheUserName();
 }
-
 
 /* Calls the database and get all user in the system and send the data to another function. */
 function getTheUserDatabase() {
@@ -23,7 +22,6 @@ function getTheUserDatabase() {
         dataType:'json',
         success:function (data) {
             WelcomeAndInformation(data.payload);
-            getTheUserName()
         }
     });
 }
@@ -41,15 +39,14 @@ function WelcomeAndInformation(AllUsers) {
         /* Find the right username and print out all information about the user. */
         if(Username == AllUsers[i].username)
         {
-            $('#startbox').append('<h1 id="startbox">'+ "Welcome " + AllUsers[i].first_name + " "+
+            $('#startbox').append('<h1 id="startbox">'+ "You are log in as admin and your name is " + AllUsers[i].first_name + " "+
                 AllUsers[i].last_name + "!");
 
             $('#informationSection2').append('<h4 id="startbox">'+ "Username:" +AllUsers[i].username +"<br>"+
                 "First name:"+ AllUsers[i].first_name + "<br>" +
                 "Last name:"+ AllUsers[i].last_name + "<br>" +
                 "Phone number:"+ AllUsers[i].phone + "<br>" +
-                "Email:"+ AllUsers[i].email + "<br>" +
-                "Card number: **** **** **** 7023");
+                "Email:"+ AllUsers[i].email + "<br>");
         }
     }
 }
@@ -64,44 +61,3 @@ window.onclick = function(event) {
     }
 }
 /*----------------------------------------------------------------------------------------------------------*/
-
-
-/*--------------------------------------Information about user balance---------------------------------------*/
-
-/*Function that take the current username and send it forward to another function.*/
-function getTheUserName() {
-    var Username = localStorage.TempUser;
-    getTheUserPurchasesHistory(Username);
-}
-
-/*The function calls the database with the current username and get all purchase history from the user.*/
-function getTheUserPurchasesHistory(Username) {
-    var link = 'http://pub.jamaica-inn.net/fpdb/api.php?username='+Username+'&password='+Username+'&action=iou_get';
-    console.log(link);
-    $.ajax({
-        type:'GET',
-        url:link,
-        dataType:'json',
-        success:function (data) {
-            Information(data.payload);
-        }
-    });
-}
-
-/* The function attached the user purchase information to the page. */
-function Information(Balance) {
-    for(var i=0; i<Balance.length; i++)
-    {
-        $('#balanceSection2').append('<p>' + "Balance:" + Balance[i].assets +"$");
-    }
-}
-
-// Get the modal
-var modal = document.getElementById('id02');
-
-// When the user clicks anywhere outside of the modal, close it.
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
